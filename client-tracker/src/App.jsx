53544a09eb8f791b9f70606a74e83939cdd6085a
Mouhyadine <zakmouh123@gmail.com>
2025-11-03 10:38:31 +0000

    initialisation of client tracker by vite+react and creation of basic Header component

---
+++ client-tracker/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Header />
+        <main style={{ padding:"20px"}}>
+          <p>Bienvenue-commence a construire ton Client Tracker</p>
+        </main>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ client-tracker/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header style={{ padding: "12px 20px",borderBottom:"1px solid #eee"}}>
+            <h1>Client Tracker</h1>
+        </header>
+    )
+}
